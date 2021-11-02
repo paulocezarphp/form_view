d9ed88020d3bc5bcc3a5bc8912b13d93586b2093
Cezar Leigos <cezarleigos28@outlook.com>
2021-11-02 20:27:15 -0300

    v0.03 - Mais algumas mudanças
    Dessa vez fizemos mais algumas pequenas mudanças e começamos também a definir algumas funções que farão parte do nosso formulário.

---
+++ app/js/script-form-view.js
@@ -0,0 +1,23 @@
+function fv_validate_null(){
+    
+    //verifica se o campo esta vazio
+
+}
+
+function fv_validate_string(){
+
+	//verifica o número de caracteres de um input
+
+}
+
+function fv_validate_email(){
+    
+    //verifica se o email é valido
+
+}
+
+function fv_validate_date(){
+   
+    //verifica se a data é valida
+
+}
